5c436d4274fcb8846f30d0500ecdb6e248fb8999
Greg Brimble <developer@gregbrimble.com>
2020-05-15 11:53:03 +0100

    Add deploy API token

--- packages/glenstack.com/src/components/tailwind.js
+++ packages/glenstack.com/src/components/tailwind.js
@@ -1,21 +1,17 @@
-import {SafeAreaView, View, Text} from 'react-native';
-import tailwind from 'tailwind-rn';
-import React from "react"
+import { SafeAreaView, View, Text } from "react-native";
+import tailwind from "tailwind-rn";
+import React from "react";
 
 const Tailwind = () => (
-    <SafeAreaView style={tailwind('h-full')}>
-        <View style={tailwind('pt-12 items-center')}>
-            <View style={tailwind('bg-blue-200 px-3 py-1 rounded-full')}>
-                <Text style={tailwind('text-blue-800 font-semibold')}>
-                    Hello Tailwind
-                </Text>
-            </View>
-        </View>
-    </SafeAreaView>
+  <SafeAreaView style={tailwind("h-full")}>
+    <View style={tailwind("pt-12 items-center")}>
+      <View style={tailwind("bg-blue-200 px-3 py-1 rounded-full")}>
+        <Text style={tailwind("text-blue-800 font-semibold")}>
+          Hello Tailwind
+        </Text>
+      </View>
+    </View>
+  </SafeAreaView>
 );
 
-
-
-
-
-export default Tailwind
+export default Tailwind;
