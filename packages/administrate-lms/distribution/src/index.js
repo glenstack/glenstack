765e3be5071485153b78e9e54c4a9d68797e87ae
Greg Brimble <developer@gregbrimble.com>
2020-06-29 00:23:51 +0100

    Add clickable links

--- packages/administrate-lms/distribution/src/index.js
+++ packages/administrate-lms/distribution/src/index.js
@@ -8,6 +8,8 @@ export const handleEvent = (event) => {
   if (request.method.toUpperCase() !== "GET") return NOT_FOUND;
 
   const url = new URL(request.url);
+  const ANDROID_LINK = `exp://${url.host}/android-index.json`;
+  const IOS_LINK = `exp://${url.host}/ios-index.json`;
 
   switch (url.pathname) {
     case "/":
@@ -16,9 +18,9 @@ export const handleEvent = (event) => {
 <html>
 <body>
 <h1>Android</h1>
-<img src="/android.png" />
+<a href="${ANDROID_LINK}"><img src="/android.png" /></a>
 <h1>iOS</h1>
-<img src="/ios.png" />
+<a href="${IOS_LINK}"><img src="/ios.png" /></a>
 </body>
 </html>`,
         {
@@ -26,12 +28,11 @@ export const handleEvent = (event) => {
         }
       );
     case "/android.png":
-      return new Response(
-        qr.imageSync(`exp://${url.host}/android-index.json`),
-        { headers: { "Content-Type": "image/png" } }
-      );
+      return new Response(qr.imageSync(ANDROID_LINK), {
+        headers: { "Content-Type": "image/png" },
+      });
     case "/ios.png":
-      return new Response(qr.imageSync(`exp://${url.host}/ios-index.json`), {
+      return new Response(qr.imageSync(IOS_LINK), {
         headers: { "Content-Type": "image/png" },
       });
   }
